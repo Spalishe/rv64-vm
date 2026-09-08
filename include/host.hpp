fd1d7b1ea52c6cd35f64192cc16078b5078157cc
Spalishe <maksamoshkin2010@gmail.com>
2026-09-08 15:27:49 +0300

    Added _M_ARM64 to hosts

--- include/host.hpp
+++ include/host.hpp
@@ -23,6 +23,6 @@ Copyright 2026 Spalishe
 #if defined(__x86_64) || defined(__x86_64__) || defined(_M_X64)
 #define HOST_TARGET_X86_64 1
 #endif
-#if defined(__aarch64__) || defined(__arm64__)
+#if defined(__aarch64__) || defined(__arm64__) || defined(_M_ARM64)
 #define HOST_TARGET_AARCH64 1
 #endif
